d597135fe13b76a10e205caf0268d8f551c3c400
Dmitry <pro100pro10010@gmail.com>
2023-02-11 00:01:45 +0300

    Add [program execution draft] main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::process::{Command, Stdio};
+use std::time::Duration;
+use wait_timeout::ChildExt;
+
 fn main() {
-    println!("Hello, world!");
+    let mut child = Command::new("python")
+        .arg("./programs/test.py")
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    let one_sec = Duration::from_secs(1);
+    let status_code = match child.wait_timeout(one_sec).unwrap() {
+        Some(status) => status.code(),
+        None => {
+            println!("Time limit!");
+            child.kill().unwrap();
+            child.wait().unwrap().code()
+        }
+    };
+
+    let output = child.wait_with_output().expect("Failed to read stdout");
+    let stdout = String::from_utf8_lossy(&output.stdout);
+
+    print!("code: {}\nout: {}", status_code.unwrap(), stdout);
 }
